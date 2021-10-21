b9c845d423869b50150e96b33a3dacca95aef704
@MagnaMonstrum <moboy98@gmail.com>
2021-10-22 00:18:40 +0200

    made dictionary

--- ProgBlokA/twitterCasus/tweetCasus.py
+++ ProgBlokA/twitterCasus/tweetCasus.py
@@ -3,33 +3,40 @@ def userMsg():
   msg = input('voer een bericht in: ')
   name = input('voer naam in: ')
 
-  data =  '{}\n{}\n{}\n'.format(stationName, msg, name)
+  data =  f'{stationName}\n{name}\n'
+
+  msgData = f'{msg}\n'
 
   if name == '':
       name = 'anoniem'
 
   newMsg = open("ProgBlokA/twitterCasus/generated/tweetbericht.txt", 'w')
 
+  newMsg.write(msgData)
   newMsg.write(data)
   newMsg.close()
 
 
 def moderate():
   msg = open("ProgBlokA/twitterCasus/generated/tweetbericht.txt", 'r')
-  userMsgLines = msg.readlines()
-  print(userMsgLines)
+  # userMsgLines = msg.readlines()
+  msgString = msg.readline()
+  print(msgString)
+  # print(userMsgLines)
+  msg.close()
   modVerdict = input('accept msg? ')
 
   if modVerdict == 'accept':
     msgAppend = open("ProgBlokA/twitterCasus/generated/tweetbericht.txt", 'a')
     msgAppend.write('Accepted')
     print('Accepted')
+    msg.close()
     
   elif modVerdict == 'reject':
     msgAppend = open("ProgBlokA/twitterCasus/generated/tweetbericht.txt", 'a')
     msgAppend.write('Rejected')
     print('Rejected')
-
+    msg.close()
 
 
 userMsg()
---
+++ ProgBlokA/twitterCasus/tweetDict.py
@@ -0,0 +1,33 @@
+reiziger = {
+    "kaartnummer":"",
+    "naam":"",
+    "wachtwoord":"",
+    "emailadres":"",
+}
+
+moderator = {
+    "personeelsnummer":"",
+    "naam":"",
+    "loon":"",    
+}
+
+bericht = {
+    "ID":"",
+    "inhoud":"",
+    "datum":"",
+    "tijd":"",
+}
+
+opmerking = {
+    "ID bericht":"",
+    "ID personeelsnummer":"",
+    "inhoud":"",
+    "datum":"",
+    "tijd":"",
+}
+
+station = {
+    "naam":"",
+    "locatie":"",
+    "schermen":"",
+}
